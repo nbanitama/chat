169e91fc3407dd435c5d8442d051b2e51a959442
Novandi Banitama <novandi.banitama@tokopedia.com>
2019-07-27 23:42:16 +0700

    add base for server and client (#1)
    [core] add simple server and client

---
+++ server/init.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"net"
+	"time"
+
+	"github.com/nbanitama/chat/util"
+)
+
+// Start represents to run a server
+func Start() {
+	service := ":7777"
+	tcpAddress, err := net.ResolveTCPAddr("tcp4", service)
+	if err != nil {
+		util.Debug("got %v", err)
+	}
+
+	listener, err := net.ListenTCP("tcp", tcpAddress)
+	if err != nil {
+		util.Debug("got %v", err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			util.Debug("got %v", err)
+			continue
+		}
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	daytime := time.Now().String()
+	conn.Write([]byte(daytime))
+}
